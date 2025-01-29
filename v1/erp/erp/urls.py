23850dced7a90a46066ea35d6038f8822f001e12
gekika <gkkaranja2@gmail.com>
2025-01-29 18:13:57 +0300

    urls.py updated

--- v1/erp/erp/urls.py
+++ v1/erp/erp/urls.py
@@ -14,10 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.contrib import admin
+# from django.urls import path ,include
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('inventory/', include('inventory.urls')),
+# ]
+
 from django.contrib import admin
-from django.urls import path ,include
+from django.urls import path, include
+from django.http import HttpResponse
+
+# def homepage(request):
+#     return HttpResponse("<h1> GreenTech</h1>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inventory/', include('inventory.urls')),
+    # path('', homepage),  # to handle the root URL
 ]
--- v1/erp/inventory/urls.py
+++ v1/erp/inventory/urls.py
@@ -1,9 +1,28 @@
+# from django.urls import path
+# from .views import ProductListView, AddStockView, RecordSaleView, ReportView
+
+# urlpatterns = [
+#     path('products/', ProductListView.as_view(), name='product-list'),
+#     path('add-stock/', AddStockView.as_view(), name='add-stock'),
+#     path('record-sale/', RecordSaleView.as_view(), name='record-sale'),
+#     path('reports/', ReportView.as_view(), name='reports'),
+# ]
+
+
+
 from django.urls import path
-from .views import ProductListView, AddStockView, RecordSaleView, ReportView
+from .views import (
+    ProductListView,
+    AddStockView,
+    RecordSaleView,
+    ReportView,
+    HomePageDataView
+)
 
 urlpatterns = [
     path('products/', ProductListView.as_view(), name='product-list'),
     path('add-stock/', AddStockView.as_view(), name='add-stock'),
     path('record-sale/', RecordSaleView.as_view(), name='record-sale'),
     path('reports/', ReportView.as_view(), name='reports'),
+    path('homepage-data/', HomePageDataView.as_view(), name='homepage-data'),  # New API
 ]
